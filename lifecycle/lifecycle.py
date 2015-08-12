d7c58ee960ff1440611b76c29bbdeb3f4bd98abb
Jake Mooney <jmooney@fandm.edu>
2015-08-12 11:21:44 -0400

    everything seems to work

--- lifecycle/lifecycle.py
+++ lifecycle/lifecycle.py
@@ -1,20 +1,24 @@
 import unirest
 import json
 
-class Lifecycle
+class Lifecycle:
 
     def __init__(self, api_key):
         self.apiKey = api_key
 
-    def identify(params):
-        response = unirest.post("http://localhost:3400/v1/identify", headers={ "Content-Type": "application/json", "lifecycle-api-key":  },
+    def getApiKey(self):
+        return self.apiKey
+    def identify(self, params):
+        response = unirest.post("http://localhost:3400/v1/identify", headers={ "Content-Type": "application/json", "lifecycle-api-key": self.apiKey},
           params=json.dumps(params)
         )
+        return response
 
-    def track(event_id, unique_id):
-        response = unirest.post("http://localhost:3400/v1/track", headers={ "Content-Type": "application/json", "lifecycle-api-key":  },
+    def track(self, event_id, unique_id):
+        response = unirest.post("http://localhost:3400/v1/track", headers={ "Content-Type": "application/json", "lifecycle-api-key": self.apiKey},
           params=json.dumps({
             "event_id": event_id,
             "unique_id": unique_id
           })
         )
+        return response
